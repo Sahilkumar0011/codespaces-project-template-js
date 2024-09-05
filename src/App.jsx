70afda6653021efa62eeba1174c0affd084d06a6
Sahilkumar0011 <114636616+Sahilkumar0011@users.noreply.github.com>
2024-09-05 19:08:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Sahil kumar",
+  title: "Web Developer",
+  email: "sallukumar0011@gmail.com",
+  gitHub: "https://github.com/Sahilkumar0011",
+  linkedIn: "https://www.linkedin.com/in/sahil-kumar-082031251/",
 };
 
 const primaryColor = "#4E567E";
